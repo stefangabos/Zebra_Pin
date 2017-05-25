8e249ddcfee5353e47cb8903e2b2b5551852f7c0
Stefan Gabos <contact@stefangabos.ro>
2017-05-25 23:49:26 +0300

    Added the "copy" task to the "watch" task

--- Gruntfile.js
+++ Gruntfile.js
@@ -95,7 +95,7 @@ module.exports = function(grunt) {
         'watch': {
             js: {
                 files: ['src/zebra_pin.src.js'],
-                tasks: ['newer:eslint', 'newer:jshint', 'newer:uglify', 'notify:done'],
+                tasks: ['newer:eslint', 'newer:jshint', 'newer:uglify', 'copy', 'notify:done'],
                 options: {
                     livereload: true
                 }
