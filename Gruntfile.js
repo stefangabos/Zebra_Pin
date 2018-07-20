694a9fc76017fa82c6e0fa627251fbf81dd1ff1d
Stefan Gabos <contact@stefangabos.ro>
2018-07-20 14:05:59 +0300

    Fixed typo

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
             done: {
                 options: {
                     title: 'Grunt ',
-                    message: 'All taks were successfully completed!'
+                    message: 'All tasks were successfully completed!'
                 }
             }
         },
