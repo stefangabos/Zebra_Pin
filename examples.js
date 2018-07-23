593b347b4c7331611a95095028ccc2c436934095
Stefan Gabos <contact@stefangabos.ro>
2018-07-23 13:25:26 +0300

    Updated examples

--- examples.js
+++ examples.js
@@ -1,27 +1,36 @@
 $(document).ready(function() {
 
-    // Pin #1
-    var pin = new $.Zebra_Pin($('#pin1'), {
-            top_spacing: 10,
+    // using a timeout because of the syntax highlighter which alters content
+    setTimeout(function() {
+
+        new $.Zebra_Pin($('.sticky-sidebar'), {
+            top_spacing: 10
+        });
+
+        new $.Zebra_Pin($('.sticky-navigation'), {
             contain: true
-        }),
-        $container = $('.pin1-container'),
-        additional_height = -300;
+        });
 
-    $('button').on('click', function() {
-        additional_height = additional_height * -1;
-        $container.css('height', '+=' + additional_height);
-        pin.update();
-    });
+        new $.Zebra_Pin($('.sticky-navigation-highlight'), {
+            contain: true
+        });
 
-    // Pin #2
-    new $.Zebra_Pin($('#pin2'), {
-        top_spacing: 10
-    });
+        new $.Zebra_Pin($('.sticky-navigation-callbacks'), {
+            contain: true,
+            onPin: function(scroll, $element) {
+                $('li', $element).eq(2).addClass('hidden');
+                $('li', $element).eq(3).removeClass('hidden');
+            },
+            onUnpin: function(scroll, $element) {
+                $('li', $element).eq(2).removeClass('hidden');
+                $('li', $element).eq(3).addClass('hidden');
+            }
+        });
 
-    // Pin #3
-    new $.Zebra_Pin($('#pin3'), {
-        hard: true
-    });
+        new $.Zebra_Pin($('.sticky-navigation-nocallbacks'), {
+            contain: true
+        });
+
+    }, 500);
 
 });
