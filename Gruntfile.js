8471043ba2d7ce2f3c77ccdb8f997d852595d042
Stefan Gabos <contact@stefangabos.ro>
2018-08-06 08:59:23 +0300

    Updates required by the latest version of Uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,7 +69,10 @@ module.exports = function(grunt) {
             options: {
                 compress: true,
                 mangle: true,
-                beautify: false
+                beautify: false,
+                output: {
+                    ie8: true
+                }
             },
             build: {
                 src: 'src/zebra_pin.src.js',
